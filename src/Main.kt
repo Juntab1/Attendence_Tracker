b18086cba7457e730e4d1f48f81c8d8a54cef864
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2023-12-15 17:47:05 -0800

    start

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Hello World!")
+}
+
+// A note keeping app focused on keeping track of people information
+//      features:
+//          - able to enter date and keep log of people information within it
+//              ex: Jun
+//                      12/15/2023
+//          - able to select a user and date to be prompted a list of options to choose from to add in
+//              actitivity, mood, etc.
+// Steps to achieve
+//      create a class of person to keep user info within it
+//      do formatting in main to start out with
