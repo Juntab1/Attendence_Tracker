ed1c2b10e7d43921a5f7b52bae8520d2b316eb25
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2023-12-18 22:19:05 -0800

    created a community class to keep track of input names for users

--- src/Community.kt
+++ src/Community.kt
@@ -1,2 +1,26 @@
 class Community {
+    private lateinit var nameList : MutableList<Person>
+
+    // set called "add"
+    fun add(name : Person) {
+        if (!::nameList.isInitialized){
+            nameList = mutableListOf<Person>()
+        }
+
+        nameList.add(name)
+    }
+
+    // get
+    fun get() : MutableList<Person> {
+        return nameList
+    }
+
+    // need to return in string version
+    fun to_string() : String {
+        var names : String = "Names: "
+        nameList.forEach(){
+            names += "$it,"
+        }
+        return names
+    }
 }
